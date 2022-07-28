5aa3859f9d942ad556fc67cdb8471c0c5eff71d3
GabrielRM198 <gabrielrochamagalhaes0@gmail.com>
2022-07-28 00:05:28 -0300

    Calculadora Vai na Web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const Calculadora = styled.div`
+
+  background-color: grey;
+  width: 35%;
+  margin: 0 auto;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 50px;
+  border: 4px solid black;
+
+  input{
+    padding: 0.5rem;
+    border-radius: 10px;
+    border: none;
+    outline: none;
+    background-color: black;
+  color: white;
+  }
+
+  
+  input[type="number"]::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
 }
 
-export default App;
+button{
+  padding: 0.5rem;
+  margin: 0.5rem;
+  border-radius: 10px;
+  border: none;
+  background-color: black;
+  color: white;
+}
+
+
+
+`
+export const Titulo = styled.h2`
+
+color: black;
+  
+`
+
+
+export default class App extends Component{
+  state = {
+
+    inp1: '',
+    inp2: '',
+    resul: ''
+    
+  }
+
+  soma = () => {
+    this.setState({
+      resul: this.state.inp1 + this.state.inp2
+    })
+  }
+
+  subtracao = () => {
+    this.setState({
+      resul: this.state.inp1 - this.state.inp2
+    })
+  }
+
+  multiplicacao = () => {
+    this.setState({
+      resul: this.state.inp1 * this.state.inp2
+    })
+  }
+
+  divisao = () => {
+    this.setState({
+      resul: this.state.inp1 / this.state.inp2
+    })
+  }
+
+  clear = () => {
+    this.setState({
+      inp1: '',
+      inp2: '',
+      resul: ''
+    })
+  }
+
+  input1 = (event) => 
+  {this.setState({inp1: Number(event.target.value)})}
+
+  input2 = (event) => 
+  {this.setState({inp2: Number(event.target.value)})}
+
+
+
+  render(){
+    return(
+        <Calculadora>
+          <Titulo>Calculadora</Titulo>
+          <input onChange={this.input1} type="number" />
+          <input onChange={this.input2} type="number" />
+          <div>
+            <button onClick={this.soma}>+</button>
+            <button onClick={this.subtracao}>-</button>
+            <button onClick={this.multiplicacao}>*</button>
+            <button onClick={this.divisao}>/</button>
+            <button onClick={this.clear}>c</button>
+          </div>
+          <h3>{this.state.resul}</h3>
+        </Calculadora>
+      
+    )
+  }
+} 
